d618d2f109001fe5354042697624cc6956b6a4d3
Ryan Kang <rkang626@gmail.com>
2021-01-17 18:04:58 +0000

    fix wordsearch

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -35,4 +35,27 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'R', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'A', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'N', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'K', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the letter array is empty", function() {
+    const result = wordSearch([], 'FRANK')
+
+    assert.isFalse(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+    let transposedMatrix = [];
+    const transposedRows = matrix[0].length;
+  
+    for (let i = 0; i < transposedRows; i++) {
+      let transposedRow = [];
+      for (const row of matrix) {
+        transposedRow.push(row[i]);
+      }
+      transposedMatrix.push(transposedRow);
     }
+  
+    return transposedMatrix;
+  };
+
+const wordSearch = (letters, word) => {
+    if (letters.length < 1) {
+        return false;
+    }
+    
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (const h of horizontalJoin) {
+        if (h.includes(word)) return true;
+    }
+
+    const transposedMatrix = transpose(letters);
+    const verticalJoin = transposedMatrix.map(ls => ls.join(''));
+    for (const v of verticalJoin) {
+        if (v.includes(word)) return true;
+    }
+
+    return false;
 }
 
 module.exports = wordSearch
